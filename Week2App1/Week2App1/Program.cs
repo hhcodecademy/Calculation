eaf37b95c55c32e8f104852c2c7cb2d1c0cd9e98
Huseyn Hasanli <huseyn.hasanli@code.edu.az>
2024-01-21 17:46:01 +0400

    add first  c# file

---
+++ Week2App1/Week2App1/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Week2App1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            float firstNumber;
+            float secondNumber;
+
+            Console.WriteLine("Please enter first Number");
+            firstNumber= float.Parse( Console.ReadLine());
+
+            Console.WriteLine("Please enter second Number");
+            secondNumber = float.Parse(Console.ReadLine());
+
+            float resultSum = firstNumber + secondNumber;
+
+            Console.WriteLine("Addition result: "+resultSum);
+
+            float resultMinus = firstNumber - secondNumber;
+
+            Console.WriteLine("Minus result: "+resultMinus);
+
+            float resultMultipl = firstNumber * secondNumber;
+
+            Console.WriteLine("Multipl result: " + resultMultipl);
+
+            float resultDivide = firstNumber / secondNumber;
+            Console.WriteLine("Divide result: " + resultDivide);
+
+        }
+    }
+}
