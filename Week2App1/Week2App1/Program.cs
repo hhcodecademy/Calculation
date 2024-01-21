4e2f61b639f8c409107a586b4767c1b719801824
Nermin Abbaszade <narminma@code.edu.az>
2024-01-21 19:33:56 +0400

    second assignment

--- Week2App1/Week2App1/Program.cs
+++ Week2App1/Week2App1/Program.cs
@@ -9,7 +9,7 @@
             float secondNumber;
 
             Console.WriteLine("Please enter first Number");
-            firstNumber= float.Parse( Console.ReadLine());
+            firstNumber = float.Parse(Console.ReadLine());
 
             Console.WriteLine("Please enter second Number");
             secondNumber = float.Parse(Console.ReadLine());
@@ -23,26 +23,26 @@
 
             float resultSum = firstNumber + secondNumber;
 
-            Console.WriteLine("Addition result: "+resultSum);
+            Console.WriteLine("Addition result: " + resultSum);
 
             float resultMinus = firstNumber - secondNumber;
 
-            Console.WriteLine("Minus result: "+resultMinus);
+            Console.WriteLine("Minus result: " + resultMinus);
 
             float resultMultipl = firstNumber * secondNumber;
 
             Console.WriteLine("Multipl result: " + resultMultipl);
 
-            
+
             float resultDivide = firstNumber / secondNumber;
             Console.WriteLine("Divide result: " + resultDivide);
 
-            float bigResult=0;
+            float bigResult = 0;
 
             if (resultSum > bigResult)
-            {
-                bigResult= resultSum;
-            }
+            { 
+                bigResult = resultSum;
+        }
             if(resultMinus > bigResult)
             {
                 bigResult= resultMinus;
---
+++ week2app2/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace week2app2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter day of the week:");
+            int num;
+            num=int.Parse(Console.ReadLine());
+            if (num == 1) {
+                Console.WriteLine("Studying Unuversity Lesson");
+            }
+            else if (num == 2)
+            {
+                Console.WriteLine("Studying Course Lesson");
+            }
+            else if (num == 3)
+            {
+                Console.WriteLine("Going to gym");
+            }
+            else if (num == 4)
+            {
+                Console.WriteLine("Hiking");
+            }
+            else if (num == 5)
+            {
+                Console.WriteLine("Studying Piano Lesson");
+            }
+            else if (num == 6)
+            {
+                Console.WriteLine("Code Academy first day");
+            }
+            else
+            {
+                Console.WriteLine("Code academy second day");
+            }
+        }
+    }
+}
